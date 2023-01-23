8c807a7d1c6e7ff0578d68d673846488571795b9
Kota Kawanishi <kokotata421@gmail.com>
2023-01-24 05:33:33 +0900

    Initial Commit

---
+++ Sources/SwiftHTTPClient/SwiftHTTPClient.swift
@@ -0,0 +1,6 @@
+public struct SwiftHTTPClient {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SwiftHTTPClientTests/SwiftHTTPClientTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftHTTPClient
+
+final class SwiftHTTPClientTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftHTTPClient().text, "Hello, World!")
+    }
+}
